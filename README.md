34ddbf82b61d0f9cd173a524be7da37bc4c20ac6
Aftaab Dobani <1234dobani@gmail.com>
2021-03-23 23:44:32 -0400

    added button and listners

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # code-quiz
+
+
+
+choices: "Structure and Content", "Style", "Backend Development"
+choices: "String", "True/False", "Integers"
+choices: "Javascript", "CSS", "Python"
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-console.log("Hello");
+// DOM Variable 
+var startButton = document.getElementById("start-quiz");
+var quoteContainer = document.getElementById("quiz");
+
+//Javascript Variable
+var arrayOfQuestions = [
+        "What does HTML provide?",
+        "What is a boolean?",
+        "Which coding language do we not learn at GA TECH Bootcamp?",
+    ];
+var currentIndex = 0;
+
+
+
+
+//Function Variable
+function displayQuestion(){
+    quoteContainer.innerHTML = "";
+    var questionEl = document.createElement("h1");
+    questionEl.textContent = arrayOfQuestions[currentIndex];
+    quoteContainer.append(questionEl);
+
+}
+
+//Event Listeners
+startButton.addEventListener("click", function () {
+    arrayOfQuestions++;
+    displayQuestion();
+}); 
+
+
+
+//Function Calls 
+
+displayQuestion();
+
