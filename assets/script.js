4dc283d068136fc290f3af5e4ff78342c1e850d2
Aftaab Dobani <1234dobani@gmail.com>
2021-03-24 22:47:37 -0400

    tried to make buttons work and show up the questions

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,49 @@
-// DOM Variable 
+// DOM Variable
 var startButton = document.getElementById("start-quiz");
 var quoteContainer = document.getElementById("quiz");
 
 //Javascript Variable
 //array of objects for the questions
-var arrayOfQuestions = [
-    {
-        question: "What does HTML provide?",
-        choice: {
-            A:"Structure and Content", 
-            B: "Style", 
-            C:"Backend Development",
-        },
-        answer: "Structure and Content"
-    },
-    {
-        question: "What is a boolean?",
-        choices: {
-            A: "String", 
-            B: "True/False", 
-            C: "Integers",
-        },
-        answer: "String",
-    },
-    {
-        question:  "Which coding language do we not learn at GA TECH Bootcamp?",
-        choices: {
-            A:"Javascript", 
-            B:"CSS",
-            C:"Python",
-        },
-        answer: "Python",
-    }
-   
-]
-var currentIndex = 0;
-
-
-
+var Questions = [
+  {
+    question: "What does HTML provide?",
+    choice: ["Structure and Content", "Style", "Backend Development"],
+    answer: "Structure and Content",
+  },
+  {
+    question: "What is a boolean?",
+    choice: ["String", "True/False", "Integers"],
+    answer: "String",
+  },
+  {
+    question: "Which coding language do we not learn in class?",
+    choice: ["Javascript", "CSS", "Python"],
+    answer: "Python",
+  },
+];
+var currentIndex = 2;
+var currentChoice = 2;
 
 //Function Variable
-function displayQuestion(){
-    quoteContainer.innerHTML = "";
-    var questionEl = document.createElement("h1");
-    questionEl.textContent = arrayOfQuestions[currentIndex];
-    quoteContainer.append(questionEl);
+function displayQuestion() {
+  quoteContainer.innerHTML = "";
+  var questionEl = document.createElement("h1");
+  questionEl.textContent = Questions[currentIndex].question;
+  quoteContainer.append(questionEl);
 
+  function displayChoices() {
+    quoteContainer.innerHTML = "";
+    var choiceEl = document.createElement("h2");
+    choiceEl.textContent = Questions[currentChoice].choice;
+    quoteContainer.append(quoteEl);
+  }
 }
 
 //Event Listeners
 startButton.addEventListener("click", function () {
-   currentIndex++;
-    displayQuestion();
-}); 
+  displayQuestions();
+});
 
-
-
-
-//Function Calls 
+//Function Calls
 
 displayQuestion();
-
